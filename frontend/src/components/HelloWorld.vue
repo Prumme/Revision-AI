3e6d12683d425268d4b7b06ed9e913803b904635
Ilyam Dupuis <ilyamdupuis0903@gmail.com>
2025-03-13 12:16:29 +0100

    Initial commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1 class="text-red-500">Hello World</h1>
+    <Apple />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Apple } from "lucide-vue-next";
+</script>
