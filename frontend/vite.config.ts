1ccdac280b8fe0d42c1a9d7f484b068ea2cfff18
Prumme <aurelien23.p@gmail.com>
2025-04-05 11:34:24 +0200

    :pencil: creation d'un docker compose afin de faire tourner le front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,16 +9,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    eslint(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), eslint(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  server: {
+    host: true,
+    port: 5173,
+  },
+});
