b8a30713611c071e1601a3f005a6405ac255faff
Prumme <aurelien23.p@gmail.com>
2025-04-08 09:58:21 +0200

    ✨ create user sidenav

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,10 +1,13 @@
-<template>
-  <div class="container">
-    <h1 class="text-red-500">Hello World</h1>
-    <Apple />
-  </div>
-</template>
-
 <script setup lang="ts">
 import { Apple } from "lucide-vue-next";
 </script>
+
+<template>
+  <RouterLink
+    to="/#"
+    class="flex items-center gap-5 px-4 py-3 rounded-lg hover:bg-primary hover:inset-shadow-md duration-200"
+  >
+    <House />
+    <span>Accueil</span>
+  </RouterLink>
+</template>
---
+++ frontend/src/components/buttons/NavLink.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import DynamicIcon from "@/components/icons/DynamicIcon.vue";
+
+const {to, icon, text, active} = defineProps<{
+  to: string,
+  icon: string,
+  text: string,
+  active: boolean
+}>()
+
+</script>
+
+<template>
+  <RouterLink
+    :to="to"
+    class="flex items-center gap-5 px-4 py-3 rounded-lg"
+    :class="{'hover:bg-primary  duration-200': !active, 'bg-primary ': active}"
+  >
+    <DynamicIcon :name="icon" />
+    <span>{{ text }}</span>
+  </RouterLink>
+</template>
