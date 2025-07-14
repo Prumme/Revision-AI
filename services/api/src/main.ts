91935489fb72c90a716fee14c05c180329384b02
axel <axel@reality-academy.fr>
2025-07-14 19:53:22 +0200

    fix cors

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: '*',
+    origin: process.env.CORS_ORIGIN || '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
